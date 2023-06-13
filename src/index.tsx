6331fd4f65988241eab4ef5089fe90d857eafc98
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-06-13 16:52:36 +0300

    Extract config keys to standalone variables

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,14 @@ import store from './slices';
 import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
 
-const firebaseConfig = JSON.parse(
-  process.env.REACT_APP_FIREBASE_CONFIG as string
-);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 console.log(firebaseApp);
